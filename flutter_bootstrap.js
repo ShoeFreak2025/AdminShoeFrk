6688c62089c0e1963bb06824a60d527170592969
MarkSangil <marksangil.bsit2019@gmail.com>
2025-10-15 22:49:03 +0800

    Update website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3859185394"
+    serviceWorkerVersion: "1957700114"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5a68dc8a0a747c2e0410f46a6d075838",
+const RESOURCES = {"flutter_bootstrap.js": "1bb80f44046cdc8b7d504b8ba62a7ad5",
 "version.json": "d4b54d6aea4a0d3e7bda2442c7508a47",
 "index.html": "a06657d6bed234312ec364a8f271d9a0",
 "/": "a06657d6bed234312ec364a8f271d9a0",
-"main.dart.js": "e19a8549fc08902eb17ba663db9ce356",
+"main.dart.js": "7be4af084c4ac7ec1481c945660451d7",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "5a68dc8a0a747c2e0410f46a6d075838",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "f1973d6e1bb55e0a452dfbe5a82ae405",
+"assets/fonts/MaterialIcons-Regular.otf": "8e97f0eb9664fd0bae5c2a5d3533bbcc",
 "canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "canvaskit/skwasm_heavy.js": "413f5b2b2d9345f37de148e2544f584f",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
